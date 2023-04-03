32104bdab1ab19319eaad1468316a7a0cba58753
alessiaBer <alexdeyn@gmail.com>
2023-04-03 11:02:33 +0200

    [add] contacts search

--- MILESTONE_4/assets/js/app.js
+++ MILESTONE_4/assets/js/app.js
@@ -8,7 +8,10 @@ Milestone 2
 -Click sul contatto mostra la conversazione del contatto cliccato
 Milestone 3
 Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e digitando “enter” il testo viene aggiunto al thread sopra, come messaggio verde
-Risposta dall’interlocutore: ad ogni inserimento di un messaggio, l’utente riceverà un “ok” come risposta, che apparirà dopo 1 secondo */
+Risposta dall’interlocutore: ad ogni inserimento di un messaggio, l’utente riceverà un “ok” come risposta, che apparirà dopo 1 secondo 
+Milestone 4
+-Ricerca utenti: scrivendo qualcosa nell’input a sinistra, vengono visualizzati solo i contatti il cui nome contiene le lettere inserite 
+(es, Marco, Matteo Martina -> Scrivo “mar” rimangono solo Marco e Martina)*/
 
 
 const { createApp } = Vue
@@ -18,6 +21,7 @@ createApp({
         return {
             newMessage: '',
             activeContact: 0,
+            resultedContacts: '',
             newReceivedMessage: {
                 date: '',
                 message: 'lol',
