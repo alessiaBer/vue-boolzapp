38642dcd35e05405645a965fee8d8c46fba789f7
alessiaBer <alexdeyn@gmail.com>
2023-06-29 22:14:51 +0200

    [add] dark mode

--- BONUS/assets/js/app.js
+++ BONUS/assets/js/app.js
@@ -28,7 +28,7 @@ Milestone 5 - opzionale
 ☑️ sotto al nome del contatto nella parte in alto a destra, cambiare l'indicazione dello stato: visualizzare il testo "sta scrivendo..." nel timeout in cui il pc risponde, poi mantenere la scritta "online" per un paio di secondi e infine visualizzare "ultimo accesso alle xx:yy" con l'orario corretto
 ☑️ dare la possibilità all'utente di cancellare tutti i messaggi di un contatto o di cancellare l'intera chat con tutti i suoi dati: cliccando sull'icona con i tre pallini in alto a destra, si apre un dropdown menu in cui sono presenti le voci "Elimina messaggi" ed "Elimina chat"; cliccando su di essi si cancellano rispettivamente tutti i messaggi di quel contatto (quindi rimane la conversazione vuota) oppure l'intera chat comprensiva di tutti i dati del contatto oltre che tutti i suoi messaggi (quindi sparisce il contatto anche dalla lista di sinistra)
 ☑️ dare la possibilità all'utente di aggiungere una nuova conversazione, inserendo in un popup il nome e il link all'icona del nuovo contatto
--fare scroll in giù in automatico fino al messaggio più recente, quando viene aggiunto un nuovo messaggio alla conversazione (NB: potrebbe esserci bisogno di utilizzare nextTick - vedi documentazione Vue3)
+☑️ fare scroll in giù in automatico fino al messaggio più recente, quando viene aggiunto un nuovo messaggio alla conversazione (NB: potrebbe esserci bisogno di utilizzare nextTick - vedi documentazione Vue3)
 ☑️ aggiungere le emoticons, tramite l'utilizzo di una libreria, ad esempio: https://www.npmjs.com/package/vue-emoji-picker*/
 /* #endregion functionalities BONUS */
 /* #region graphic BONUS */
@@ -65,6 +65,7 @@ createApp({
             computerIsWriting: false,
             computerIsOnline: false,
             showEmoji: false,
+            darkMode: false,
             newReceivedMessage: {
                 date: '',
                 message: '',
